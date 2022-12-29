8db6a6351e6210a1f36f24d6c3066d281d407222
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2022-12-29 14:16:53 +0530

    #include<stdio.h> int dig(int n) {     int r,k=0;     while(n>0)     {         r=n%10;         k++;         n=n/10;     }     return k; } int main() {     int n,i;     scanf("%d",&n);     int k=dig(n);     int x[k];     for(int i=k-1;i>=0;i--)     {         (x[i]=n%10);         n=n/10; } for(i=0;i<k;i++) { if(x[i]!=9)         {             x[i]=9;             break;         }     }     for(int i=0;i<k;i++)     {         printf("%d",x[i]);     } }

---
+++ Maximum_69_Number.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int dig(int n)
+{
+    int r,k=0;
+    while(n>0)
+    {
+        r=n%10;
+        k++;
+        n=n/10;
+    }
+    return k;
+}
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int k=dig(n);
+    int x[k];
+    for(int i=k-1;i>=0;i--)
+    {
+        (x[i]=n%10);
+        n=n/10;
+}
+for(i=0;i<k;i++)
+{
+if(x[i]!=9)
+        {
+            x[i]=9;
+            break;
+        }
+    }
+    for(int i=0;i<k;i++)
+    {
+        printf("%d",x[i]);
+    }
+}
