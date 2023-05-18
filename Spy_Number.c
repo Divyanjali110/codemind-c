fa2f23806ebe70051efedc59bc5e84d0753afb43
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2023-05-18 21:29:59 +0530

    #include <stdio.h>
    int isSpyNumber(int number) {
        int sum = 0;
        int product = 1;
        int digit;
    
        while (number > 0) {
            digit = number % 10;
            sum += digit;
            product *= digit;
            number /= 10;
        }
    
        if (sum == product)
            return 1; // It is a spy number
        else
            return 0; // It is not a spy number
    }
    
    int main() {
        int number;
        scanf("%d", &number);
    
        if (isSpyNumber(number))
            printf("Spy Number
    ");
        else
            printf("Not Spy Number");
    
        return 0;
    }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,18 +1,32 @@
-#include<stdio.h>
-int main()
-{
-    int a;
-    scanf("%d",&a);
-    int r,s=0,p=1;
-    while(a!=0)
-    {
-        r=a%10;
-        a=a/10;
-        p=p*r;
-        s=s+r;
+#include <stdio.h>
+
+int isSpyNumber(int number) {
+    int sum = 0;
+    int product = 1;
+    int digit;
+
+    while (number > 0) {
+        digit = number % 10;
+        sum += digit;
+        product *= digit;
+        number /= 10;
     }
-    if(s==p)
-    printf("Spy Number");
-    else 
-    printf("Not Spy Number");
+
+    if (sum == product)
+        return 1; // It is a spy number
+    else
+        return 0; // It is not a spy number
+}
+
+int main() {
+    int number;
+    scanf("%d", &number);
+
+    if (isSpyNumber(number))
+        printf("Spy Number
+");
+    else
+        printf("Not Spy Number");
+
+    return 0;
 }
