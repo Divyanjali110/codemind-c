b67f31d0e5fbd0317ddb0c245c2970faaf795fc0
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2023-05-18 21:33:10 +0530

    #include <stdio.h>
    int isNeonNumber(int num) {
        int square = num * num;
        int sum = 0;
    
        // Calculate the sum of digits in the square
        while (square > 0) {
            sum += square % 10;
            square /= 10;
        }
    
        // Check if the sum is equal to the original number
        if (sum == num) {
            return 1;  // Neon number
        } else {
            return 0;  // Not a neon number
        }
    }
    
    int main() {
        int number;
        scanf("%d", &number);
    
        if (isNeonNumber(number)) {
            printf("Neon Number
    ");
        } else {
            printf("Not Neon Number");
        }
    
        return 0;
    }

--- Neon_Number.c
+++ Neon_Number.c
@@ -1,19 +1,33 @@
-#include<stdio.h>
-int main()
-{
-    int a;
-    scanf("%d",&a);
-    int b;
-    b=(a*a);
-    int r,s=0;
-    while (b!=0)
-    {
-        r=b%10;
-        b=b/10;
-        s=s+r;
+#include <stdio.h>
+
+int isNeonNumber(int num) {
+    int square = num * num;
+    int sum = 0;
+
+    // Calculate the sum of digits in the square
+    while (square > 0) {
+        sum += square % 10;
+        square /= 10;
     }
-    if (s==a)
-    printf("Neon Number");
-    else 
-    printf("Not Neon Number");
+
+    // Check if the sum is equal to the original number
+    if (sum == num) {
+        return 1;  // Neon number
+    } else {
+        return 0;  // Not a neon number
+    }
+}
+
+int main() {
+    int number;
+    scanf("%d", &number);
+
+    if (isNeonNumber(number)) {
+        printf("Neon Number
+");
+    } else {
+        printf("Not Neon Number");
+    }
+
+    return 0;
 }
