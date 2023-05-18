e2ee7b107832f55c85bfcf02cb8130556887bedc
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2023-05-18 21:24:37 +0530

    #include <stdio.h>
    int main() {
        char number[10];
        int i;
        scanf("%s", number);
    
        // Find the first occurrence of 6
        for (i = 0; number[i] != NULL; i++) {
            if (number[i] == '6') {
                number[i] = '9';  // Change the first 6 to 9
                break;
            }
        }
    
        printf("%s
    ", number);
    
        return 0;
    }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,36 +1,20 @@
-#include<stdio.h>
-int dig(int n)
-{
-    int r,k=0;
-    while(n>0)
-    {
-        r=n%10;
-        k++;
-        n=n/10;
-    }
-    return k;
-}
-int main()
-{
-    int n,i;
-    scanf("%d",&n);
-    int k=dig(n);
-    int x[k];
-    for(int i=k-1;i>=0;i--)
-    {
-        (x[i]=n%10);
-        n=n/10;
-}
-for(i=0;i<k;i++)
-{
-if(x[i]!=9)
-        {
-            x[i]=9;
+#include <stdio.h>
+
+int main() {
+    char number[10];
+    int i;
+    scanf("%s", number);
+
+    // Find the first occurrence of 6
+    for (i = 0; number[i] != NULL; i++) {
+        if (number[i] == '6') {
+            number[i] = '9';  // Change the first 6 to 9
             break;
         }
     }
-    for(int i=0;i<k;i++)
-    {
-        printf("%d",x[i]);
-    }
+
+    printf("%s
+", number);
+
+    return 0;
 }
