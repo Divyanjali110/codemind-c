0cad04b8dbcd2ffa50be08e13337a574c798d717
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2023-05-22 20:53:34 +0530

    #include <stdio.h>
    int factorial(int n) {
        if (n == 0 || n == 1)
            return 1;
        else
            return n * factorial(n - 1);
    }
    
    int isStrongNumber(int number) {
        int originalNumber = number;
        int sum = 0;
    
        while (number > 0) {
            int digit = number % 10;
            sum += factorial(digit);
            number /= 10;
        }
    
        return (sum == originalNumber);
    }
    
    int main() {
        int number;
    
      
        scanf("%d", &number);
    
        if (isStrongNumber(number))
            printf("The number %d is a strong number
    ", number);
        else
            printf("The number %d is not a strong number
    ", number);
    
        return 0;
    }

---
+++ Strong_Number.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int isStrongNumber(int number) {
+    int originalNumber = number;
+    int sum = 0;
+
+    while (number > 0) {
+        int digit = number % 10;
+        sum += factorial(digit);
+        number /= 10;
+    }
+
+    return (sum == originalNumber);
+}
+
+int main() {
+    int number;
+
+  
+    scanf("%d", &number);
+
+    if (isStrongNumber(number))
+        printf("The number %d is a strong number
+", number);
+    else
+        printf("The number %d is not a strong number
+", number);
+
+    return 0;
+}
